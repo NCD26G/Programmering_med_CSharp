adc8dfa127f76ad8fe7afc701ba087cfa564a4a9
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-09-02 15:02:01 +0200

    End of lesson 3

---
+++ Lektion-03/ATM/Program.cs
@@ -0,0 +1,152 @@
+﻿namespace ATM;
+
+class Program
+{
+    static int balance = 0;
+
+    static void Main()
+    {
+        // Här är vår enkla meny...
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine("För att sätta in tryck på tangenten 'd'");
+        Console.WriteLine("För att ta ut tryck på tangenten 'w'");
+        Console.WriteLine("För att se saldo tryck på tangenten 'b'");
+        Console.WriteLine("För att avsluta tryck på tangenten 'x'");
+        Console.WriteLine("--------------------------------------------------");
+
+        while (true)
+        {
+            var key = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(key) || key == "x")
+            {
+                Environment.Exit(0);
+            }
+
+            switch (key)
+            {
+                case "b":
+                    DisplayBalance();
+                    break;
+                case "d":
+                    Console.WriteLine("Hur mycket vill du sätta in?");
+                    var amount = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(amount))
+                    {
+                        Console.WriteLine("Du måste ange ett heltalsvärde");
+                        Environment.Exit(0);
+                    }
+
+                    if (int.TryParse(amount, out int result))
+                    {
+                        Deposit(result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du måste ange ett heltalsvärde för att jag ska kunna förstå!");
+                    }
+                    break;
+                case "w":
+                    Console.WriteLine("Hur mycket vill du ta ut?");
+                    amount = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(amount))
+                    {
+                        Console.WriteLine("Du måste ange ett heltalsvärde");
+                        Environment.Exit(0);
+                    }
+
+                    if (int.TryParse(amount, out int value))
+                    {
+                        WithDraw(value);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du måste ange ett heltalsvärde för att jag ska kunna förstå!");
+                    }
+                    break;
+                default:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        /*
+        if (string.IsNullOrWhiteSpace(key))
+        {
+            Environment.Exit(0);
+        }
+        else if (key == "d")
+        {
+            Console.WriteLine("Hur mycket vill du sätta in?");
+            var amount = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(amount))
+            {
+                Console.WriteLine("Du måste ange ett heltalsvärde");
+                Environment.Exit(0);
+            }
+
+            if (int.TryParse(amount, out int result))
+            {
+                Deposit(result);
+                DisplayBalance();
+            }
+            else
+            {
+                Console.WriteLine("Du måste ange ett heltalsvärde för att jag ska kunna förstå!");
+            }
+        }
+        else if (key == "w")
+        {
+            Console.WriteLine("Hur mycket vill du ta ut?");
+            var amount = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(amount))
+            {
+                Console.WriteLine("Du måste ange ett heltalsvärde");
+                Environment.Exit(0);
+            }
+
+            if (int.TryParse(amount, out int result))
+            {
+                WithDraw(result);
+                DisplayBalance();
+            }
+            else
+            {
+                Console.WriteLine("Du måste ange ett heltalsvärde för att jag ska kunna förstå!");
+            }
+        }
+        else if (key == "b")
+        {
+            DisplayBalance();
+        }
+        else if (key == "x")
+        {
+            Environment.Exit(0);
+        }
+        else
+        {
+            Environment.Exit(0);
+        }
+        
+        */
+    }
+
+    static void Deposit(int amount) // Header - Definition
+    { // Body...
+        balance += amount;
+    }
+
+    static void DisplayBalance()
+    {
+        Console.WriteLine($"Ditt nuvarande saldo: {balance}");
+    }
+
+    static void WithDraw(int amount)
+    {
+        balance -= amount;
+    }
+}
---
+++ Lektion-03/ref_out_params/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ref_out_params;
+
+class Program
+{
+    static void Main()
+    {
+        int age = 25;
+        // int result = Demo(age);
+        // int refResult = DemoRef(ref age);
+        int outResult = DemoOut(age, out int outAge, out string test);
+
+        // Console.WriteLine($"Local age: {age}");
+        // Console.WriteLine($"Returned age: {result}");
+        // Console.WriteLine("ByReference--------------------------");
+        // Console.WriteLine($"Local age: {age}");
+        // Console.WriteLine($"Returned age: {refResult}");
+        Console.WriteLine("By Out Parameter--------------------------");
+        Console.WriteLine($"Local age: {age}");
+        Console.WriteLine($"Returned age: {outResult}");
+        Console.WriteLine($"Returned out arg: {outAge}");
+        Console.WriteLine($"Returned out arg: {test}");
+    }
+
+    static int Demo(int age) // CopyByValue - Kopierar endast värdet på variabeln (25)
+    {
+        age++; // Räkna värdet på argumentet
+        return age;
+    }
+
+    static int DemoRef(ref int age) // CopyByReference - Kopiera adressen till variabeln
+    {
+        age++; // Manipulerar vi värdet på adressen
+        return age;
+    }
+
+    static int DemoOut(int age, out int newAge, out string message)
+    {
+        newAge = age;
+        newAge++;
+        message = "Här kommer tillbaka ett meddelande ifrån min metod";
+        return age;
+    }
+}
