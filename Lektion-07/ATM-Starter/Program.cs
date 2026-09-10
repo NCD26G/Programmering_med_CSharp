7d000dd6ba61b3bc8660a57ea5f4e5b8728404cc
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-09-10 21:24:19 +0200

    Added starter project for lesson 7

---
+++ Lektion-07/ATM-Starter/Program.cs
@@ -0,0 +1,119 @@
+﻿using WestcoastBank;
+
+namespace ATM;
+
+class Program
+{
+    // static Account account = new Account("1234-5678");
+    static Account account = new("1234-5678") { };
+
+    // static Account account = new() { accountNumber = "1234-5678" };
+
+    static void Main()
+    {
+        account.FirstName = "Michael";
+        // Här är vår enkla meny...
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine("För att sätta in tryck på tangenten 'd'");
+        Console.WriteLine("För att ta ut tryck på tangenten 'w'");
+        Console.WriteLine("För att se saldo tryck på tangenten 'b'");
+        Console.WriteLine("För att se transaktionerna tryck på tangenten 't'");
+        Console.WriteLine("För att avsluta tryck på tangenten 'x'");
+        Console.WriteLine("--------------------------------------------------");
+
+        App();
+    }
+
+    static void App()
+    {
+        try
+        {
+            while (true)
+            {
+                var key = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(key) || key == "x")
+                {
+                    Environment.Exit(0);
+                }
+
+                switch (key)
+                {
+                    case "b":
+                        DisplayBalance();
+                        break;
+                    case "t":
+                        DisplayTransactions();
+                        break;
+                    case "d":
+                        Console.WriteLine("Hur mycket vill du sätta in?");
+                        var amount = Console.ReadLine();
+
+                        if (string.IsNullOrWhiteSpace(amount))
+                        {
+                            throw new Exception("Du måste ange ett heltalsvärde");
+                        }
+
+                        if (!int.TryParse(amount, out int result))
+                        {
+                            throw new Exception("Du måste ange ett heltalsvärde för att jag ska kunna förstå!");
+                        }
+                        Deposit(result);
+                        break;
+                    case "w":
+                        Console.WriteLine("Hur mycket vill du ta ut?");
+                        amount = Console.ReadLine();
+
+                        if (string.IsNullOrWhiteSpace(amount))
+                        {
+                            throw new Exception("Du måste ange ett heltalsvärde");
+                        }
+
+                        if (!int.TryParse(amount, out int value))
+                        {
+                            throw new Exception("Du måste ange ett heltalsvärde för att jag ska kunna förstå!");
+                        }
+                        WithDraw(value);
+                        break;
+                    case "x":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Ditt val finns inte i menyn");
+                        break;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            App();
+        }
+        finally
+        {
+            Console.WriteLine("Klar för idag, nu är det fredag!");
+        }
+    }
+
+    static void Deposit(int amount)
+    {
+        account.Deposit(amount);
+    }
+
+    static void WithDraw(int amount)
+    {
+        account.WithDraw(amount);
+    }
+    static void DisplayBalance()
+    {
+        Console.WriteLine($"Ditt nuvarande saldo: {account.Balance}");
+    }
+
+    static void DisplayTransactions()
+    {
+        foreach (var tran in account.Transactions)
+        {
+            Console.WriteLine(tran.ToString());
+        }
+    }
+}
