7b7120ae952775f5ef6aac7fd78eb8b3339aaf4a
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-09-10 10:01:26 +0200

    Working with constructors

---
+++ Lektion-06/ATM/BankAccount.cs
@@ -0,0 +1,70 @@
+namespace WestcoastBank;
+
+enum TransactionTypeEnum
+{
+    Insättning,
+    Uttag
+}
+
+class Account
+{
+    private int _balance;
+    public string accountNumber;
+    public string firstName = "";
+    public string lastName = "";
+    public List<Transaction> transactions = [];
+
+    public Account(string accountNo)
+    {
+        accountNumber = accountNo;
+    }
+
+    public Account(string accountNo, string firstName, string lastName)
+    {
+        accountNumber = accountNo;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int GetBalance()
+    {
+        return _balance;
+    }
+    public void Deposit(int amount)
+    {
+        _balance += amount;
+        AddTransaction(amount, TransactionTypeEnum.Insättning);
+    }
+
+    public void WithDraw(int amount)
+    {
+        if (_balance < amount)
+        {
+            throw new Exception("Du har inte tillräckligt på kontot");
+        }
+        _balance -= amount;
+
+        AddTransaction(amount, TransactionTypeEnum.Uttag);
+    }
+
+    void AddTransaction(int amount, TransactionTypeEnum type)
+    {
+        var tran = new Transaction();
+        tran.transactionDate = DateTime.Now;
+        tran.transactionType = type;
+        tran.transactionAmount = amount;
+        transactions.Add(tran);
+    }
+}
+
+class Transaction
+{
+    public DateTime transactionDate;
+    public TransactionTypeEnum transactionType;
+    public int transactionAmount;
+
+    public string GetTransactionInfo()
+    {
+        return $"Transaktionsdatum: {transactionDate} Transaktionstyp: {transactionType} Belopp: {transactionAmount}";
+    }
+}
