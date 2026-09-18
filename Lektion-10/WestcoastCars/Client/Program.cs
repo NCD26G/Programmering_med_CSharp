9e1e3226035c285002a57fcca45a0a27392d4f4d
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-09-18 14:03:25 +0200

    Simplified list initialization

--- Lektion-10/WestcoastCars/Client/Program.cs
+++ Lektion-10/WestcoastCars/Client/Program.cs
@@ -7,15 +7,27 @@ class Program
 {
     static void Main()
     {
-        CarPark carPark = new();
+        CarPark carPark = new() { };
 
-        Vehicle volvo = new("Volvo", "XC60");
-        Vehicle ford = new("Ford", "Mustang MACH-E");
-        Vehicle fiat = new("Fiat", "Uno");
+        carPark.Vehicles.Add(new Vehicle("Volvo", "V60") { ModelYear = 2022 });
+        carPark.Vehicles.Add(new Vehicle("Ford", "Fusion"));
+        carPark.Vehicles.Add(new Vehicle("Fiat", "Uno"));
 
-        carPark.Vehicles.Add(volvo);
-        carPark.Vehicles.Add(ford);
-        carPark.Vehicles.Add(fiat);
+        // Vehicle bmw = new("BMW", "i4")
+        // {
+        //     ModelYear = 2022
+        // };
+
+
+        // CarPark carPark = new();
+
+        // Vehicle volvo = new("Volvo", "XC60");
+        // Vehicle ford = new("Ford", "Mustang MACH-E");
+        // Vehicle fiat = new("Fiat", "Uno");
+
+        // carPark.Vehicles.Add(volvo);
+        // carPark.Vehicles.Add(ford);
+        // carPark.Vehicles.Add(fiat);
 
         foreach (var vehicle in carPark.Vehicles)
         {
