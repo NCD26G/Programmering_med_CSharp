34246e2e6e6a32a369a11c3576b1bbd5c53513f4
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-09-18 10:11:36 +0200

    Playing with Lists

--- Lektion-10/WestcoastCars/Client/Program.cs
+++ Lektion-10/WestcoastCars/Client/Program.cs
@@ -5,17 +5,22 @@ class Program
     // Skapa en array som lagrar 3 strängar(tillverkare av bilar)
     static void Main()
     {
-        string[] vehicles = ["Volvo", "Ford", "Fiat"];
+        // Deklarerat en variabel
+        List<string> vehicles = ["Volvo", "Ford", "Fiat"];
 
         Console.WriteLine(vehicles[1]);
         // Ändra Ford till Volkswagen
         vehicles[1] = "Volkswagen";
         Console.WriteLine(vehicles[1]);
 
-        for (int i = 0; i < vehicles.Length; i++)
+        for (int i = 0; i < vehicles.Count; i++) // 0 => 1 => 2
         {
             Console.WriteLine(vehicles[i]);
         }
+
+        Console.WriteLine("==============================");
+        // Lägg till en ny tillverkare...
+        vehicles.Add("Mercedes");
 
         foreach (var vehicle in vehicles)
         {
