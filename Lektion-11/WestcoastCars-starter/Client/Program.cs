52d606e11db0772c63c8e07f8290458ce8c5dc1d
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-09-22 19:28:09 +0200

    Added starter project for lesson 11

---
+++ Lektion-11/WestcoastCars-starter/Client/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Runtime.Intrinsics.Arm;
+using Client.Models;
+
+namespace Client;
+
+class Program
+{
+    static void Main()
+    {
+        Car volvo = new("Volvo", "XC90", "ABC123")
+        {
+            Color = "Black",
+            Mileage = 105000,
+            Engine = new Engine { FuelType = "Diesel", HorsePower = 200, Volume = 2.0 }
+        };
+
+        CarPark carPark = new CarPark();
+        carPark.Vehicles.Add(volvo);
+
+        Console.WriteLine(volvo.ToString());
+        Console.WriteLine("---------------------------------------------------------");
+
+        foreach (var item in carPark.Vehicles)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ Lektion-11/WestcoastCars-starter/todo.md
@@ -0,0 +1,20 @@
+# Uppgift 3.
+
+- Skapa egenskap för hantering av Modell år (läsa och skriva)
+- Skapa ett privat fält för att lagra modell år
+- Använd den nya egenskapen i Main metoden i Program.cs
+
+# Uppgift 4.
+
+- Skapa en ny klass _CarPark_ i mappen **Models**
+- Skapa en autoimplementerad egenskap som hanterar en lista av klassen _Vehicle_
+
+# Uppgift 5.
+
+- I program.cs skapa en loop som går igenom varje bil i listan och skriver ut bilen i terminalen
+
+# Uppgift 6.
+
+- I listningen så ska ni presentera Tillverkare och Modell utskriften.
+
+# Övning inför onsdagens lektion 23/9.
