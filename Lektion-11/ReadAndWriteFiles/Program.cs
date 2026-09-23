4a0cca6b882496b694fa8b6f25fa2c488062f855
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-09-23 13:13:04 +0200

    Added how to read and write files

---
+++ Lektion-11/ReadAndWriteFiles/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ReadAndWriteFiles;
+
+class Program
+{
+    static void Main()
+    {
+
+        // Directory.Exists(Environment.CurrentDirectory + "/Data");
+        string path = Environment.CurrentDirectory + "/Data/log.txt";
+        if (File.Exists(path))
+        {
+            // Skriv till fil...
+            string message = "Log added " + DateTime.Now.ToString();
+            File.WriteAllText(path, message);
+            // -----------------------------------------------------
+            // Läs fil...
+            string text = File.ReadAllText(path);
+            Console.WriteLine(text);
+        }
+        else
+        {
+            Console.WriteLine("Fil eller katalog saknas");
+        }
+
+
+
+        /*
+        // Stream klasserna...
+        // Skriv till fil
+        using StreamWriter sw = new(path);
+        string message = "Nytt log ärende: " + DateTime.Now.ToString();
+        sw.WriteLine(message);
+
+        // Best practice...
+        sw.Close();
+
+        // Läs ifrån fil
+        using StreamReader sr = new(path);
+        string log = sr.ReadToEnd();
+        Console.WriteLine(log);
+
+        sr.Close();
+        */
+    }
+}
---
+++ Lektion-11/ReadAndWriteJson/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace ReadAndWriteJson;
+
+class Person
+{
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public required Address Address { get; set; }
+}
+
+class Address
+{
+    public string AddressLine { get; set; } = "";
+    public string PostalCode { get; set; } = "";
+    public string City { get; set; } = "";
+}
+
+class Program
+{
+    static void Main()
+    {
+        string path = Environment.CurrentDirectory + "/Data/person.json";
+        // {FirstName=Erik, LastName=Eriksson, Address={AddressLine = ?, PostalCode = ?, City = ?}}
+        Person person = new()
+        {
+            FirstName = "Erik",
+            LastName = "Eriksson",
+            Address = new()
+            {
+                AddressLine = "Gatan 1",
+                PostalCode = "123 45",
+                City = "Staden"
+            }
+        };
+
+        JsonSerializerOptions options = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true,
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+
+        string json = JsonSerializer.Serialize(person, options);
+        File.WriteAllText(path, json);
+
+        // Läs in json och gör om till objekt...
+        options = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        string personString = File.ReadAllText(path);
+        person = JsonSerializer.Deserialize<Person>(personString, options)!;
+
+        Console.WriteLine($"Förnamn: {person.FirstName}");
+    }
+}
