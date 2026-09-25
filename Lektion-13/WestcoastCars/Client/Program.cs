1e5f64da2f7ee817218ec024e865c99a4ee08d90
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-09-25 10:02:20 +0200

    Added Lesson 13

---
+++ Lektion-13/WestcoastCars/Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using Core.Models;
+
+namespace Client;
+
+class Program
+{
+    static void Main()
+    {
+        Vehicle vehicle = new("Ford");
+
+        Console.WriteLine(vehicle);
+    }
+}
---
+++ Lektion-13/WestcoastCars/Core/Models/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Models;
+
+public class Vehicle(string make)
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Manufacturer { get; set; } = make;
+    public string? Model { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id: {Id} Tillverkare: {Manufacturer} Modell: {Model}";
+    }
+}
